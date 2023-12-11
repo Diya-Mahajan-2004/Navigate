57f10e984cfc1bacd228fdf53dd89be4fdebc2c4
Diya Mahajan <145265599+Diya-Mahajan-2004@users.noreply.github.com>
2023-12-11 06:05:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Welcome to Navigate")
+
+
+
+$( ".check" ).datepicker({  changeMonth: true,
+  changeYear: true,
+  showButtonPanel: true,
+  yearRange: "-100:+0",
+  dateFormat: 'dd/mm/yy'
+});
+
+$( "#check-out" ).datepicker({  changeMonth: true,
+  changeYear: true,
+  showButtonPanel: true,
+  yearRange: "-100:+0",
+  dateFormat: 'dd/mm/yy'
+});
